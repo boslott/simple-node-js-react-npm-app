60b0c652e077154da06cccd050c34cca14d0e1e8
= <=>
2019-07-03 04:03:08 -0400

    change source code to see changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        🤪 🤪 🤪 🤪 Bo Bo Bo says Hello!! 🤪 🤪 🤪 🤪 
         </p>
       </div>
     );
